3a700c98a41487cb79793a30daa2cd6edcf5f34a
olivermrbl <oliver@mrbltech.com>
2022-11-02 19:14:15 +0100

    Start develop

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#Run migrations to ensure the database is updated
+#npm run migrations
+#seed
+#npm run seed
+#Start development environment
+npm run serve
---
+++ prod.v2.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker compose -f docker-compose.v2.prod.yml "$@"
